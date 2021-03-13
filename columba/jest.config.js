caf23cd31a1aa30e9caa01069efacb0c637f28e2
Kengo Yamashita <63894988+yamashita-kenngo@users.noreply.github.com>
2021-03-13 10:53:03 +0900

    Add: Web Serverのひな型追加 Nest.js App を追加した

--- columba/jest.config.js
+++ columba/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  projects: ['<rootDir>/apps/demo-app', '<rootDir>/apps/api'],
+  projects: [
+    '<rootDir>/apps/demo-app',
+    '<rootDir>/apps/api',
+    '<rootDir>/apps/web',
+  ],
 };
